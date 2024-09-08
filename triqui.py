0d2f4272b4fe3aaf42d767d6aac067f8b36cd565
brigith91 <bridgeth1991@gmail.com>
2024-09-07 21:06:22 -0500

    se mejoro la visual del tablero

--- triqui.py
+++ triqui.py
@@ -7,13 +7,16 @@ Tablero: list[list[str]] = [
 
 
 def imprimir_tablero(tab):
-    print('    0, 1, 2')
-    for i, fila in enumerate(tab): 
-        print(i, end=' | ')  # recorre tablero
+
+    print('     0   1   2')
+    for num_fila, fila, in enumerate(tab): 
+        print(num_fila, end=' ↾ ')  # recorre tablero
         for casilla in fila:  # recorre columnas
-            print(casilla, end='  ')  # imprime la fila
-        print('\n-----------')
-    print('    0, 1, 2')
+            print(casilla, end=' ↿ ')
+        print(num_fila)
+        if num_fila < 2:  # imprime la fila
+            print(' ----------------')
+    print('     0   1   2')
 
 
 imprimir_tablero(Tablero)

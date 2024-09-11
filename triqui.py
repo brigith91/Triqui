16ac43633f9b8e54b75e3eb3bb894e3c8429ecee
brigith91 <168245239+brigith91@users.noreply.github.com>
2024-09-11 11:23:41 -0500

    Add files via upload

--- triqui.py
+++ triqui.py
@@ -1,22 +1,42 @@
 Tablero: list[list[str]] = [
-    ['*', '*', '*', ],
-    ['*', '*', '*', ],
-    ['*', '*', '*', ],
+    ['*', '*', '*', ], #0
+    ['*', '*', '*', ], #1
+    ['*', '*', '*', ], #2
 
 ]
 
 
-def imprimir_tablero(tab):
+def imprimir_tablero(tab: list[list[str], ...])-> None
 
-    print('     0   1   2')
-    for num_fila, fila, in enumerate(tab): 
-        print(num_fila, end=' ↾ ')  # recorre tablero
+    print('    A  B   C')
+    nombres_filas = 'XYZ'
+
+    for num_fila, fila, in enumerate(tab):
+        nom_fila = nombres_filas[num_fila]
+
+        print(nom_fila, end=' ↾ ') 
+
         for casilla in fila:  # recorre columnas
             print(casilla, end=' ↿ ')
-        print(num_fila)
+
+        print(nombres_filas)
         if num_fila < 2:  # imprime la fila
             print(' ----------------')
-    print('     0   1   2')
+
+    print('    A  B   C')
+
+def leer_posicion()-> tuple[int,int]:
+    posicion: str =input("Ingrese una pocision (ej: xC:) ")
+
+    fila_str: str = posicion[0]
+    columna_str: str =posicion[1]
+
+    return fila_str, columna_str
+
 
 
 imprimir_tablero(Tablero)
+
+fila, columna =leer_posicion
+
+print(f"la posicion leida es : {fila}")
